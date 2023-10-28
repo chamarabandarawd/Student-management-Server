c481ee5191e0c28dc87efdd84de313d92cd5328a
chamarabandarawd <chamarabandarawd@gmail.com>
2023-10-28 11:24:15 +0530

    student work correctly

--- src/main/java/org/codewavelab/Main.java
+++ src/main/java/org/codewavelab/Main.java
@@ -1,9 +1,16 @@
 package org.codewavelab;
 
+import org.codewavelab.model.Student;
+import org.codewavelab.service.StudentService;
+
+import java.util.ArrayList;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        StudentService service =new StudentService();
+        ArrayList<Student> students=service.getStudentList();
+        System.out.println(students);
     }
 }
--- src/main/java/org/codewavelab/service/StudentService.java
+++ src/main/java/org/codewavelab/service/StudentService.java
@@ -1,4 +1,19 @@
 package org.codewavelab.service;
 
+import org.codewavelab.model.Student;
+
+import java.util.ArrayList;
+
 public class StudentService {
+
+    public ArrayList<Student> getStudentList(){
+        ArrayList<Student> studentList = new ArrayList<>();
+
+        //Creating dummy student List
+        //TODO: Integrate with database to actual students
+        studentList.add(Student.builder().firstName("Chamara").LastName("Bandara").contactNumber("0719130861").build());
+        studentList.add(Student.builder().firstName("Sumudu").LastName("Nishadi").contactNumber("0759130868").build());
+        return studentList;
+    }
+
 }
